4e2031712aa44254cb6c93719a01faf7dd6473e2
devdoyeon <doyeonyou@naver.com>
2024-04-22 03:44:22 +0900

    scrollbar setting

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,21 @@
-import { useEffect, useState } from 'react';
 import main from 'images/mainPhoto.jpg';
+import scrollDownIcon from 'images/scrollDown.svg';
+import mainLogo from 'images/dearcho.svg'
+import sign from 'images/sign.png'
 
 const MainPage = () => {
-
   return (
     <div className='container'>
-      <img src={main} alt='메인이미지' />
+      <div className='first'>
+        <img src={main} alt='메인이미지' className='mainImg' />
+        <img src={mainLogo} alt="메인 로고" className='mainLogo' />
+        <img src={sign} alt="싸인" className='sign' />
+        <img
+          src={scrollDownIcon}
+          alt='스크롤 다운 아이콘'
+          className='scroll-down-icon'
+        />
+      </div>
     </div>
   );
 };
