053d1639b6bd9e94dc51fab4260c2077902738df
devdoyeon <doyeonyou@naver.com>
2024-04-25 11:13:01 +0900

    add album page

---
+++ src/pages/AlbumPage.jsx
@@ -0,0 +1,87 @@
+import { useEffect } from "react";
+import $ from "jquery";
+import albumBg from "images/albumBg.jpg";
+import album00 from "images/albumCover/0.jpg";
+import album01 from "images/albumCover/1.jpg";
+import album02 from "images/albumCover/2.jpg";
+import album03 from "images/albumCover/3.jpg";
+import album04 from "images/albumCover/4.jpg";
+import album05 from "images/albumCover/5.jpg";
+import album06 from "images/albumCover/6.jpg";
+import album07 from "images/albumCover/7.jpg";
+import album08 from "images/albumCover/8.jpg";
+import album09 from "images/albumCover/9.jpg";
+import album10 from "images/albumCover/10.jpg";
+import album11 from "images/albumCover/11.jpg";
+import albumInfo from "data/albumInfo.json";
+
+const AlbumPage = ({ scrollY }) => {
+  const albumArr = [
+    album00,
+    album01,
+    album02,
+    album03,
+    album04,
+    album05,
+    album06,
+    album07,
+    album08,
+    album09,
+    album10,
+    album11,
+  ];
+
+  const renderAlbumList = () => {
+    return albumArr.map((albumCover, idx) => {
+      return (
+        <>
+          <img src={albumCover} alt={idx} className="album-cover" />
+        </>
+      );
+    });
+  };
+
+  useEffect(() => {
+    if (scrollY > $(".album-title").offset().top - (window.innerHeight - 200))
+      $(".album-title").addClass("animate");
+    if (scrollY > $(".album-wrap").offset().top - (window.innerHeight - 200))
+      $(".album-wrap").addClass("animate");
+  }, [scrollY]);
+
+  return (
+    <div className="container albumPage">
+      <h2 className="title album-title odd">ALBUM</h2>
+      <img src={albumBg} alt="앨범 배경이미지" className="pageImg" />
+      <div className="column album-wrap">
+        <span className="click-guide">CLICK ME!</span>
+        <div className="row album-list">{renderAlbumList()}</div>
+        <div className="row scroll-wrap">
+          <button
+            className="scroll-btn prev"
+            onClick={() =>
+              document.querySelector(".album-list").scrollBy({
+                left: -(window.innerWidth / 2),
+                behavior: "smooth",
+              })
+            }
+          >
+            &#60;
+          </button>
+          <button
+            className="scroll-btn next"
+            onClick={() =>
+              document.querySelector(".album-list").scrollBy({
+                left: window.innerWidth / 2,
+                behavior: "smooth",
+              })
+            }
+          >
+            &#62;
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AlbumPage;
--- src/pages/IntroducePage.jsx
+++ src/pages/IntroducePage.jsx
@@ -1,93 +1,41 @@
-import { useEffect, useState } from 'react';
-// import SwiperCore, { Navigation } from 'swiper';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/swiper.css';
-// import 'swiper/css/navigation';
-// import YouTube from 'react-youtube';
-// import { isBrowser, isMobile } from 'react-device-detect';
+import { useEffect } from "react";
+import $ from "jquery";
+import introBg from "images/introBg.jpg";
 
-const IntroducePage = () => {
-  // SwiperCore.use([Navigation]);
-
-  // const idArr = [
-  //   '6b-2rqPr6gE',
-  //   'EuTwMSnwYeI',
-  //   '_L55mXoIRjk',
-  //   'HPXald8Nr2g',
-  //   'UhETtNInxIs',
-  // ];
-
-  const [info, setInfo] = useState('');
-
+const IntroducePage = ({ scrollY }) => {
   useEffect(() => {
-    console.log(info);
-  }, [info]);
+    if (scrollY > $(".info-title").offset().top - (window.innerHeight - 200))
+      $(".info-title").addClass("animate");
+    if (scrollY > $(".info-box").offset().top - (window.innerHeight - 200))
+      $(".info-box").addClass("animate");
+  }, [scrollY]);
 
   return (
-    <div className='container intro'>
-      <h2 className='title'>INFO</h2>
-      <div className='column info-box'>
-        <div
-          className='row'
-          onMouseEnter={() => setInfo('name')}
-          onMouseLeave={() => setInfo('')}>
+    <div className="container introducePage">
+      <h2 className="title info-title even">INTRODUCE</h2>
+      <img src={introBg} alt="메인이미지" className="introduceBg pageImg" />
+      <div className="column info-box">
+        <div className="row">
           <span>이름</span>
           <h4>조원상</h4>
         </div>
-        <div
-          className='row'
-          onMouseEnter={() => setInfo('birth')}
-          onMouseLeave={() => setInfo('')}>
+        <div className="row">
           <span>생년월일</span>
           <h4>1996.08.15</h4>
         </div>
-        <div
-          className='row'
-          onMouseEnter={() => setInfo('company')}
-          onMouseLeave={() => setInfo('')}>
+        <div className="row">
           <span>소속사</span>
           <h4>MYSTIC STORY</h4>
         </div>
-        <div
-          className='row'
-          onMouseEnter={() => setInfo('group')}
-          onMouseLeave={() => setInfo('')}>
+        <div className="row">
           <span>소속 그룹</span>
           <h4>LUCY</h4>
         </div>
-        <div
-          className='row'
-          onMouseEnter={() => setInfo('position')}
-          onMouseLeave={() => setInfo('')}>
+        <div className="row">
           <span>포지션</span>
-          <h4>프로듀싱 / 베이스</h4>
+          <h4>프로듀싱 & 베이스</h4>
         </div>
       </div>
-      {/* <h2 className='title'>YOUTUBE</h2>
-      <Swiper
-        className='main-swiper'
-        loop={true}
-        modules={[Navigation]}
-        centeredSlides={true}
-        allowTouchMove={!isBrowser}
-        navigation={isBrowser}>
-        {idArr?.map(id => {
-          return (
-            <SwiperSlide>
-              <YouTube
-                videoId={id}
-                opts={{
-                  width: `${window.innerWidth - 600}`,
-                  height: `${window.innerHeight - 400}`,
-                  playerVars: {
-                    modestbranding: 0,
-                  },
-                }}
-              />
-            </SwiperSlide>
-          );
-        })}
-      </Swiper> */}
     </div>
   );
 };
