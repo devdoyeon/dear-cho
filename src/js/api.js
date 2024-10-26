579fe7b21a958eef0bd35340f01cb66a56255d47
devdoyeon <doyeonyou@naver.com>
2024-10-26 16:43:18 +0900

    delete proxy

--- src/js/api.js
+++ src/js/api.js
@@ -5,7 +5,7 @@ export const sample = async () => {
   const channelId='UC_OvuF5_Ce6LUi9ddd0ywAw'
   try {
     return await axios.get(
-      `/api/search?${options}&channelId=${channelId}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+      `https://www.googleapis.com/youtube/v3/search?${options}&channelId=${channelId}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
     );
   } catch (error) {
     return console.log(error);
